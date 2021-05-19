098ff960b34820230ee0dd3b34886b6b6b61c877
samade123 <samuel.ademola43@gmail.com>
2021-05-19 06:48:15 +0100

    DAC added

--- main.c
+++ main.c
@@ -24,16 +24,25 @@ int main(void)
 	RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
 
 	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; //Direct pulses to clock timer
+	//DAC Start-up Procedure:
+	RCC->APB1ENR |= RCC_APB1ENR_DAC1EN;
 
-	// GPIOE is a structure defined in stm32f303xc.h file
-	// Define settings for each output pin using GPIOE structure
 	// GPIOE->MODER |= 0x00010000; // Set mode of each pin in port E
 	GPIOE->MODER |= 0x55550000;		// Set ouput mode of  pin 8-15 so all are output mode
+	GPIOA->MODER |= 0x00000A00;		// Set ouput mode of  pin 4 and 5 to analogue mode
 	GPIOE->OTYPER &= ~(0x00000100); // Set output type for each pin required in Port E(open drain for pin 8 and push pull for pin 12)
 	GPIOE->PUPDR &= ~(0x55550000);	// Set Pull up/Pull down resistor configuration for Port E(pin 8 and pin 12 both set to pull up resistor)
-	// TIM3->CR1 &= ~CEN; // Disable TIM7 interrupt
+									// TIM3->CR1 &= ~CEN; // Disable TIM7 interrupt
 
-	TIM3->PSC = 799;   // prescalor value in Timer ‘x’ as 100
+	// Disable the ‘buffer’ function in the DAC control register
+	DAC1->CR |= DAC_CR_BOFF1;
+
+	// Enable DAC peripheral
+	DAC1->CR |= DAC_CR_EN1;
+
+	// DAC1->DAC1_DHR12R1
+
+	TIM3->PSC = 799;  // prescalor value in Timer ‘x’ as 100
 	TIM3->ARR = 9999; // Auto-Reset Register of Timer ‘x’ set to 1000 counts
 	// ‘Update’ Interrupt Enable (UIE) – or 0x00000001
 	TIM3->CR1 |= TIM_CR1_CEN;
@@ -51,12 +60,15 @@ void TIM3_IRQHandler()
 {
 	if ((TIM3->SR & TIM_SR_UIF) != 0) // Check interrupt source is from the ‘Update’ interrupt flag
 	{
- 		if (a > 0x0000)
+		if (a > 0x0000)
 		{
-			GPIOE->ODR ^= a<<8;		 // turn LEds off
+			GPIOE->ODR ^= a << 8;		  // turn LEds off
+			DAC1->DHR12R1 ^= a << 8; // turn LEds off
 		}
 		a = a + 1;
-		GPIOE->ODR ^= a<<8; // toggle LED state
+		GPIOE->ODR ^= a << 8;		  // toggle LED state
+		DAC1->DHR12R1 ^= a << 8; // turn LEds off
+
 		if (a > max)
 		{
 			a = 0x0000; // reset led counter
