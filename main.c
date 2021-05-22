52389f41f6fa0bf2c05d4c93008ce3e18204dcdd
samade123 <samuel.ademola43@gmail.com>
2021-05-22 20:56:27 +0100

    lab3.1

--- main.c
+++ main.c
@@ -29,25 +29,61 @@ int main(void)
 
 	// Enable clock on GPIO port E
 	RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
-	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; //Direct pulses to clock timer
+	// RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; //Direct pulses to clock timer
+	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
 
 	// GPIOE->MODER |= 0x00010000; // Set mode of each pin in port E
-	GPIOE->MODER |= 0x55550000;		// Set ouput mode of  pin 8-15 so all are output mode
+	// GPIOE->MODER |= 0x55550000;		// Set ouput mode of  pin 8-15 so all are output mode
+	GPIOE->MODER |= 0x00080000;		// Set ouput mode of  pin 9 to alternate function mode
 	GPIOE->OTYPER &= ~(0x00000100); // Set output type for each pin required in Port E(open drain for pin 8 and push pull for pin 12)
 	GPIOE->PUPDR &= ~(0x55550000);	// Set Pull up/Pull down resistor configuration for Port E(pin 8 and pin 12 both set to pull up resistor)
 									// TIM3->CR1 &= ~CEN; // Disable TIM7 interrupt
-	DAC_init();
-	ADC_init();
-	ADC_start();
 
-	TIM3->PSC = 799;  // prescalor value in Timer ‘x’ as 100
-	TIM3->ARR = 9999; // Auto-Reset Register of Timer ‘x’ set to 1000
+	GPIOE->AFR[1] |= 0x0000020; //Setting PE.9 to output TIM1_CH1 is obtained using the following command
 
+	// DAC_init();
+	// ADC_init();
+	// ADC_start();
+
+	// TIM3->PSC = 799;  // prescalor value in Timer ‘x’ as 100
+	// TIM3->ARR = 9999; // Auto-Reset Register of Timer ‘x’ set to 1000
+
+	TIM1->PSC = 799; // prescalor value in Timer ‘x’ as 100
+	TIM1->ARR = 9;	 // Auto-Reset Register of Timer ‘x’ set to 1000
+	// 100hz = 8MHZ/(9+1)*(799+1)
+
+	TIM1->CCMR1 |= 0x00000060; // The followed sets channel 1 to be standard PWM mode:
+	TIM1->CCR1 = 10;		   //Sets on time to 10 clock pulses
+
+	TIM1->BDTR |= TIM_BDTR_MOE;
+	TIM1->CCER |= TIM_CCER_CC1E; //. The following enables output for Channel 1:
+
+	TIM1->CR1 |= TIM_CR1_CEN; //Enable Timer
 	// ‘Update’ Interrupt Enable (UIE) – or 0x00000001
-	TIM3->CR1 |= TIM_CR1_CEN;
-	TIM3->DIER |= TIM_DIER_UIE; // Set DIER register to watch out for an
+	// TIM3->CR1 |= TIM_CR1_CEN;
+	// TIM3->DIER |= TIM_DIER_UIE; // Set DIER register to watch out for an
 
-	NVIC_EnableIRQ(TIM3_IRQn); // Enable Timer ‘x’ interrupt request in NVIC
+	// NVIC_EnableIRQ(TIM3_IRQn); // Enable Timer ‘x’ interrupt request in NVIC
+
+	// // // GPIOE->MODER |= 0x00010000; // Set mode of each pin in port E
+	// // GPIOE->MODER |= 0x55550000;		// Set ouput mode of  pin 8-15 so all are output mode
+	// GPIOE->MODER |= 0x00080030;		// Set ouput mode of  pin 8-15 so all are output mode
+	// GPIOE->OTYPER &= ~(0x00000100); // Set output type for each pin required in Port E(open drain for pin 8 and push pull for pin 12)
+	// GPIOE->PUPDR &= ~(0x55550000);	// Set Pull up/Pull down resistor configuration for Port E(pin 8 and pin 12 both set to pull up resistor)
+	// // 								// TIM3->CR1 &= ~CEN; // Disable TIM7 interrupt
+
+	// // TIM3->PSC = 799;  // prescalor value in Timer ‘x’ as 100
+	// // TIM3->ARR = 9999; // Auto-Reset Register of Timer ‘x’ set to 1000
+	// GPIOE->AFR[0]|=0x00000200;
+	// // GPIOE->AFR[0]|=0x00000200;
+
+	// TIM1->PSC = 79;  // prescalor value in Timer ‘x’ as 100
+	// TIM1->ARR = 9; // Auto-Reset Register of Timer ‘x’ set to 1000
+	// TIM1->CCR1=4;
+
+	// TIM1->PSC = 79;  // prescalor value in Timer ‘x’ as 100
+	// TIM1->ARR = 9; // Auto-Reset Register of Timer ‘x’ set to 1000
+	// TIM1->CCR1=4;
 
 	// Main programme loop - make LED 4 (attached to pin PE.0) turn on and off
 	while (1)
@@ -100,11 +136,11 @@ void ADC_start(void)
 }
 void ADC_init(void)
 {
-	ADC1->CR &= ~ADC_CR_ADVREGEN; // reset state
+	ADC1->CR &= ~ADC_CR_ADVREGEN;  // reset state
 	ADC1->CR |= ADC_CR_ADVREGEN_0; // enable state
 
 	int i = 0;
-	while (i <200) //delay for 10 usecond
+	while (i < 200) //delay for 10 usecond
 	{
 		// cout << i << "\n";
 		i++;
@@ -120,21 +156,20 @@ void ADC_init(void)
 	RCC->AHBENR |= RCC_AHBENR_ADC12EN;	   // Enable ADC1 clock
 	ADC1_2_COMMON->CCR |= 0x00010000;
 
-
 	// ADC Channel configuration PC1 in analog mode
 	RCC->AHBENR |= RCC_AHBENR_GPIOCEN; // GPIOC Periph clock enable
 	GPIOC->MODER |= 0x0000000C;		   // Configure ADC Channel7 as analog input
 
 	// ADC configuration
-	ADC1->CFGR |= ~ADC_CFGR_CONT;   // 0: Single conversion mode
-	ADC1->CFGR &= ADC_CFGR_RES_1; // 8-bit data resolution
+	ADC1->CFGR |= ~ADC_CFGR_CONT;  // 0: Single conversion mode
+	ADC1->CFGR &= ADC_CFGR_RES_1;  // 8-bit data resolution
 	ADC1->CFGR &= ~ADC_CFGR_ALIGN; // Right data alignment
 
 	/* ADC1 regular channel7 configuration */
 	ADC1->SQR1 |= ADC_SQR1_SQ1_2 | ADC_SQR1_SQ1_1 | ADC_SQR1_SQ1_0; // SQ1 = 0x07, start converting ch7
 	ADC1->SQR1 &= ~ADC_SQR1_L;										// ADC regular channel sequence length = 0 => 1 conversion/sequence
-	ADC1->SMPR1 |= ADC_SMPR1_SMP7_1 | ADC_SMPR1_SMP7_0;	 //011: 7.5 ADC clock cycles			// = 0x03 => sampling time 7.5 ADC clock cycles
-	ADC1->CR |= ADC_CR_ADEN; // Enable ADC1 - SET ADEN high in ADCx_CR
+	ADC1->SMPR1 |= ADC_SMPR1_SMP7_1 | ADC_SMPR1_SMP7_0;				//011: 7.5 ADC clock cycles			// = 0x03 => sampling time 7.5 ADC clock cycles
+	ADC1->CR |= ADC_CR_ADEN;										// Enable ADC1 - SET ADEN high in ADCx_CR
 	while (!ADC1->ISR & ADC_ISR_ADRD)
 		; //• Wait for ADRDY flag in ADC1_ISR
 }
@@ -145,7 +180,7 @@ void DAC_init()
 	RCC->APB1ENR |= RCC_APB1ENR_DAC1EN;
 	GPIOA->MODER |= 0x00000F00; // Set ouput mode of  pin 4 and 5 to analogue mode
 	// GPIOA->MODER |= 0x00000A00; // Set ouput mode of  pin 4 and 5 to analogue mode
-								// Disable the ‘buffer’ function in the DAC control register
+	// Disable the ‘buffer’ function in the DAC control register
 	DAC1->CR |= DAC_CR_BOFF1;
 	// Enable DAC peripheral
 	DAC1->CR |= DAC_CR_EN1;
