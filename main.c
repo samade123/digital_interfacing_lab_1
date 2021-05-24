4ead6f474eb2f2d9066d0a8815a178e4e3ac5a09
samade123 <samuel.ademola43@gmail.com>
2021-05-24 10:38:37 +0100

    created encoder on pin8 and 9: lab 3.3.1

--- main.c
+++ main.c
@@ -1,86 +1,77 @@
-/*
-	Pete Hubbard 2019
-	Loughborough University
-	WSC055 Lab 1
-	V.2.0
-
-	The following 'c' code presents an outline for you to adapt during the laboratory
-
-	*/
-
 // PSC = 799
 // ARR =99999
 int a = 0x0000;
 int max = 0x00ff;
-// int i;
+int state = 0;
+#define PIN9 0x0002u
+#define PIN8 0x0001u
 
 #include "stm32f3xx.h" // Device header
-
-// void TIM3_IRQHandler(void);
-void ADC_init(void);
-void DAC_init(void);
-void intr_btn(void);
 
 int main(void)
 {
 
 	// Enable clock on GPIO port E
 	RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
-	// RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; //Direct pulses to clock timer
-	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; //Direct pulses to clock timer
+	// RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
 
 	// GPIOE->MODER |= 0x00010000; // Set mode of each pin in port E
 	// GPIOE->MODER |= 0x55550000;		// Set ouput mode of  pin 8-15 so all are output mode
-	GPIOE->MODER |= 0x08880000;		// Set ouput mode of  pin 9-10 to alternate function mode
+	GPIOE->MODER |= 0x00050000;		// Set ouput mode of  pin 8-9 to output mode
 	GPIOE->OTYPER &= ~(0x00000100); // Set output type for each pin required in Port E(open drain for pin 8 and push pull for pin 12)
 	GPIOE->PUPDR &= ~(0x55550000);	// Set Pull up/Pull down resistor configuration for Port E(pin 8 and pin 12 both set to pull up resistor)
-									// TIM3->CR1 &= ~CEN; // Disable TIM7 interrupt
 
-	GPIOE->AFR[1] |= 0x0202020; //Setting PE.8-10 to output TIM1_CH1 is obtained using the following command
-	intr_btn();
+	TIM3->PSC = 799;  // prescalor value in Timer ‘x’ as 100
+	TIM3->ARR = 9999; // Auto-Reset Register of Timer ‘x’ set to 1000 counts
+	// setting timer interrupt to every 1 second
 
-	TIM1->PSC = 799; // prescalor value in Timer ‘x’ as 100
-	TIM1->ARR = 9;	 // Auto-Reset Register of Timer ‘x’ set to 1000
-	// 100hz = 8MHZ/(9+1)*(799+1)
+	TIM3->CR1 |= TIM_CR1_CEN;
+	TIM3->DIER |= TIM_DIER_UIE; // Set DIER register to watch out for an
+	GPIOE->ODR ^= a << 8; // turn LEds off
 
-	TIM1->CCMR1 |= 0x00006060; // The followed sets channel 1-2 to be standard PWM mode:
-	TIM1->CCMR2 |= 0x00000060; // The followed sets channel 3 to be standard PWM mode:
-	TIM1->CCR1 = 1;			   //Sets on time to 10 clock pulses
-	TIM1->CCR2 = 5;
-	TIM1->CCR3 = 9;
-	TIM1->BDTR |= TIM_BDTR_MOE;
-	TIM1->CCER |= TIM_CCER_CC1E | TIM_CCER_CC2E | TIM_CCER_CC3E; //. The following enables output for Channel 1:s
-
-	TIM1->CR1 |= TIM_CR1_CEN; //Enable Timer
+	NVIC_EnableIRQ(TIM3_IRQn); // Enable Timer ‘x’ interrupt request in NVIC
 
 	while (1)
 	{
 	}
 }
 
-void intr_btn(void)
+void TIM3_IRQHandler()
 {
-	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; //Enable the system configuration controller to be connected to a system clock
-	EXTI->IMR |= EXTI_IMR_MR0;			  // . The following unmasks EXTI0:
-	EXTI->RTSR |= EXTI_RTSR_TR0;		  //. The following sets EXTI0 to generate an interrupt through a rising edge:
-	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PA;
-	NVIC_EnableIRQ(EXTI0_IRQn); // Enable Timer ‘x’ interrupt request in NVIC
-	NVIC_SetPriority(EXTI0_IRQn, 100);
-}
-
-void EXTI0_IRQHandler()
-{
-	if (EXTI->PR & EXTI_PR_PR0) // check source
+	if ((TIM3->SR & TIM_SR_UIF) != 0) // Check interrupt source is from the ‘Update’ interrupt flag
 	{
-		EXTI->PR |= EXTI_PR_PR0; // clear flag*
-		if (TIM1->CCR1 == 10) // increment intensity of led.
+		// if (a > 0x0000)
+		// {
+		// 	GPIOE->ODR ^= a << 8; // turn LEds off
+		// }
+		// a = a + 1;
+		// GPIOE->ODR ^= a << 8; // toggle LED state
+		// if (a > max)
+		// {
+		// 	a = 0x0000; // reset led counter
+		// }
+
+		switch (state)
 		{
-			TIM1->CCR1 = 0;
-		}
-		else
-		{
-			TIM1->CCR1 += 1;
+		case 0:
+			GPIOE->BSRRH = 0x00000100; // turn LEds off
+			state = state + 1;
+			break;
+		case 1:
+			GPIOE->BSRRH  = 0x00000200; // turn LEds off
+			state = state + 1;
+			break;
+		case 2:
+			GPIOE->BSRRL = 0x00000100; // turn LEds off
+			state = state + 1;
+			break;
+		case 3:
+			GPIOE->BSRRL = 0x00000200; // turn LEds off
+			state = 0;
+			break;
 		}
 	}
-};
-
+	TIM3->SR &= ~TIM_SR_UIF; // Reset ‘Update’ interrupt flag in the SR register
+							 // GPIOE->ODR ^= a;		 // toggle LED state
+}
