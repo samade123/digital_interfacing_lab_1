e232cf744ab05cd900296484708fc29230464726
samade123 <samuel.ademola43@gmail.com>
2021-05-25 20:58:14 +0100

    attempt to finsih lab 3.3.2

--- main.c
+++ main.c
@@ -1,26 +1,39 @@
+
+#include "stm32f3xx.h" // Device header
+#include "stdbool.h"
 // PSC = 799
 // ARR =99999
 int a = 0x0000;
+int counter = 0x0000;
 int max = 0x00ff;
-int state = 0;
+int state, store_odr, new_odr, old_odr = 0;
+bool pin8_state, pin9_state = false;
+#define PIN14 0x0040u
+#define PIN13 0x0020u
+#define PIN12 0x0010u
+#define PIN11 0x0008u
+#define PIN10 0x0004u
 #define PIN9 0x0002u
 #define PIN8 0x0001u
-
-#include "stm32f3xx.h" // Device header
+void ext_itr_enable(void);
+void counterDecrement(void);
+void counterIncrement(void);
 
 int main(void)
 {
 
 	// Enable clock on GPIO port E
 	RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;	// Enable clock on GPIO port B
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;	// Enable clock on GPIO port B
 	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; //Direct pulses to clock timer
-	// RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
 
 	// GPIOE->MODER |= 0x00010000; // Set mode of each pin in port E
-	// GPIOE->MODER |= 0x55550000;		// Set ouput mode of  pin 8-15 so all are output mode
-	GPIOE->MODER |= 0x00050000;		// Set ouput mode of  pin 8-9 to output mode
-	GPIOE->OTYPER &= ~(0x00000100); // Set output type for each pin required in Port E(open drain for pin 8 and push pull for pin 12)
+	GPIOE->MODER |= 0x55550000;		// Set ouput mode of  pin 8-15 so all are output mode
+	GPIOE->OTYPER &= ~(0x00000000); // Set output type for each pin required in Port E(open drain for pin 8 and push pull for pin 12)
 	GPIOE->PUPDR &= ~(0x55550000);	// Set Pull up/Pull down resistor configuration for Port E(pin 8 and pin 12 both set to pull up resistor)
+
+	GPIOB->MODER |= 0x00000000; // set all pins on port B to input mode(not actually needed) we will be using pin b0 and b1
 
 	TIM3->PSC = 799;  // prescalor value in Timer ‘x’ as 100
 	TIM3->ARR = 9999; // Auto-Reset Register of Timer ‘x’ set to 1000 counts
@@ -28,10 +41,11 @@ int main(void)
 
 	TIM3->CR1 |= TIM_CR1_CEN;
 	TIM3->DIER |= TIM_DIER_UIE; // Set DIER register to watch out for an
-	GPIOE->ODR ^= a << 8; // turn LEds off
+	GPIOE->ODR ^= a << 8;		// turn LEds off
 
 	NVIC_EnableIRQ(TIM3_IRQn); // Enable Timer ‘x’ interrupt request in NVIC
 
+	ext_itr_enable();
 	while (1)
 	{
 	}
@@ -39,39 +53,137 @@ int main(void)
 
 void TIM3_IRQHandler()
 {
-	if ((TIM3->SR & TIM_SR_UIF) != 0) // Check interrupt source is from the ‘Update’ interrupt flag
+	if ((TIM3->SR & TIM_SR_UIF) != 0) // Use interrupt to gnerate encoder sequence on pins 8 and 9
 	{
-		// if (a > 0x0000)
-		// {
-		// 	GPIOE->ODR ^= a << 8; // turn LEds off
-		// }
-		// a = a + 1;
-		// GPIOE->ODR ^= a << 8; // toggle LED state
-		// if (a > max)
-		// {
-		// 	a = 0x0000; // reset led counter
-		// }
-
 		switch (state)
 		{
 		case 0:
-			GPIOE->ODR ^= PIN8 << 8; // turn LEds off
+			GPIOE->BSRRL = old_odr << 8;				// reset odr count
+			GPIOE->ODR ^= (PIN8 << 8) | (new_odr << 8); // turn pin 8 on and update count
+			// GPIOE->ODR ^= PIN8 << 8; // turn pin 8 on
 			state = state + 1;
 			break;
 		case 1:
-			GPIOE->ODR ^= PIN9 << 8; // turn LEds off
+			GPIOE->BSRRL = old_odr << 8;				// reset odr count
+			GPIOE->ODR ^= (PIN9 << 8) | (new_odr << 8); // turn LEds off
+			// GPIOE->ODR ^= PIN9 << 8; // turn pin 0 on
 			state = state + 1;
 			break;
 		case 2:
-			GPIOE->ODR ^= PIN8 << 8; // turn LEds off
+			GPIOE->BSRRL = old_odr << 8;				// reset odr count
+			GPIOE->ODR ^= (PIN8 << 8) | (new_odr << 8); // turn LEds off
+			// GPIOE->ODR ^= PIN8 << 8;					// turn pin 8 off
 			state = state + 1;
 			break;
 		case 3:
-			GPIOE->ODR ^= PIN9 << 8; // turn LEds off
+			GPIOE->BSRRL = old_odr << 8;				// reset odr count
+			GPIOE->ODR ^= (PIN8 << 8) | (new_odr << 8); // turn LEds off
+			// GPIOE->ODR ^= PIN9 << 8;					// turn pin 9 0ff
 			state = 0;
 			break;
 		}
 	}
 	TIM3->SR &= ~TIM_SR_UIF; // Reset ‘Update’ interrupt flag in the SR register
 							 // GPIOE->ODR ^= a;		 // toggle LED state
+}
+
+void ext_itr_enable(void) //enabling interrupts on pin PB0
+{
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; //Enable the system configuration controller to be connected to a system clock
+
+	EXTI->IMR |= EXTI_IMR_MR0 | EXTI_IMR_MR15; // . The following unmasks EXTI0  EXTI15:
+
+	EXTI->RTSR |= EXTI_RTSR_TR0 | EXTI_RTSR_TR15; //. The following sets EXTI0 and EXTI15 to generate an interrupt through a rising edge:
+	EXTI->FTSR |= EXTI_FTSR_TR0 | EXTI_FTSR_TR15; //. The following sets EXTI0 and EXTI15 to generate an interrupt through a falling edge:
+
+	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PB;  // select exti0 as pB0
+	SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI15_PB; // select exti0 as pB1
+	NVIC_EnableIRQ(EXTI0_IRQn);					   // Configure the NVIC to trigger the interrupt service routine
+	NVIC_EnableIRQ(EXTI15_10_IRQn);				   // Configure the NVIC to trigger the interrupt service routine
+	NVIC_SetPriority(EXTI0_IRQn, 100);
+	NVIC_SetPriority(EXTI15_10_IRQn, 101);
+}
+
+void EXTI0_IRQHandler() //pin b0 connected to pin e9 (channel B)
+{
+	if (EXTI->PR & EXTI_PR_PR0) // check source
+	{
+		EXTI->PR = EXTI_PR_PR0; // clear flag*
+
+		// GPIOE->ODR ^= PIN11 << 8; // turn LEds off
+		if ((GPIOA->IDR & (PIN8 << 8)) == (PIN8 << 8))
+		{ //if pin 8 is high
+			pin9_state = true;
+		}
+		else
+		{
+			pin9_state = false;
+		}
+
+		if (pin8_state ^ pin9_state)
+		{
+			counterDecrement();
+		}
+		else
+		{
+			counterIncrement();
+		}
+	}
+};
+
+void EXTI15_10_IRQHandler() //pin b15 connected to pin e8(channel A)
+{
+	if (EXTI->PR & EXTI_PR_PR15) // check source
+	{
+		EXTI->PR = EXTI_PR_PR15; // clear flag*
+
+		// GPIOE->ODR ^= PIN10 << 8; // turn LEds off
+
+		if ((GPIOA->IDR & (PIN8 << 8)) == (PIN8 << 8))
+		{ //if pin 8 is high
+			pin8_state = true;
+		}
+		else
+		{
+			pin8_state = false;
+		}
+
+		if (pin8_state ^ pin9_state)
+		{
+			counterIncrement();
+		}
+		else
+		{
+			counterDecrement();
+		}
+	}
+};
+
+void counterIncrement(void)
+{
+	if (counter == 15)
+	{
+		counter = 0;
+	}
+	else
+	{
+		counter = counter + 1;
+	}
+	old_odr = new_odr; //get previosuly on leds
+	new_odr = counter + PIN11 - 1;
+}
+
+void counterDecrement(void)
+{
+	if (counter == 0)
+	{
+		counter = 15;
+	}
+	else
+	{
+		counter = counter - 1;
+	}
+	old_odr = new_odr; //get previosuly on leds
+	new_odr = counter + PIN11 - 1;
+	// GPIOE->ODR = new_odr << 8 | store_odr;
 }
